d70dc0ecc57a61ea37f486be536178dac01c0db3
Gawsan <67488752+Gawsan@users.noreply.github.com>
2021-12-06 23:15:34 +0530

    React Login page

---
+++ Day01/react-login-form-page/dist/script.js
@@ -0,0 +1,43 @@
+class App extends React.Component {
+  render() {
+    return /*#__PURE__*/(
+      React.createElement("div", { className: "container" }, /*#__PURE__*/
+      React.createElement("div", { className: "form-box" }, /*#__PURE__*/
+      React.createElement("div", { className: "header-form" }, /*#__PURE__*/
+      React.createElement("h4", { className: "text-primary text-center" }, /*#__PURE__*/React.createElement("i", { className: "fa fa-user-circle", style: { fontSize: "110px" } })), /*#__PURE__*/
+      React.createElement("div", { className: "image" })), /*#__PURE__*/
+
+
+      React.createElement("div", { className: "body-form" }, /*#__PURE__*/
+      React.createElement("form", null, /*#__PURE__*/
+      React.createElement("div", { className: "input-group mb-3" }, /*#__PURE__*/
+      React.createElement("div", { className: "input-group-prepend" }, /*#__PURE__*/
+      React.createElement("span", { className: "input-group-text" }, /*#__PURE__*/React.createElement("i", { class: "fa fa-user" }))), /*#__PURE__*/
+
+      React.createElement("input", { type: "text", className: "form-control", placeholder: "Username" })), /*#__PURE__*/
+
+      React.createElement("div", { className: "input-group mb-3" }, /*#__PURE__*/
+      React.createElement("div", { className: "input-group-prepend" }, /*#__PURE__*/
+      React.createElement("span", { className: "input-group-text" }, /*#__PURE__*/React.createElement("i", { class: "fa fa-lock" }))), /*#__PURE__*/
+
+      React.createElement("input", { type: "text", className: "form-control", placeholder: "Password" })), /*#__PURE__*/
+
+      React.createElement("button", { type: "button", className: "btn btn-secondary btn-block" }, "LOGIN"), /*#__PURE__*/
+      React.createElement("div", { className: "message" }, /*#__PURE__*/
+      React.createElement("div", null, /*#__PURE__*/React.createElement("input", { type: "checkbox" }), " Remember ME"), /*#__PURE__*/
+      React.createElement("div", null, /*#__PURE__*/React.createElement("a", { href: "#" }, "Forgot your password")))), /*#__PURE__*/
+
+
+      React.createElement("div", { className: "social" }, /*#__PURE__*/
+      React.createElement("a", { href: "#" }, /*#__PURE__*/React.createElement("i", { className: "fab fa-facebook" })), /*#__PURE__*/
+      React.createElement("a", { href: "#" }, /*#__PURE__*/React.createElement("i", { className: "fab fa-twitter-square" })), /*#__PURE__*/
+      React.createElement("a", { href: "#" }, /*#__PURE__*/React.createElement("i", { className: "fab fa-google" })))))));
+
+
+
+
+
+  }}
+
+
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById('root'));
